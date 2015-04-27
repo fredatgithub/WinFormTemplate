34139a435ffc95f8c50c946b7c3e1da3d0ace77d
Freddy Juhel <domaine75@gmail.com>
2015-04-27 09:45:51 +0200

    Initial commit

---
+++ WinFormTemplate/AboutBoxApplication.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace WinFormTemplate
+{
+  partial class AboutBoxApplication : Form
+  {
+    public AboutBoxApplication()
+    {
+      InitializeComponent();
+      this.Text = String.Format("À propos de {0}", AssemblyTitle);
+      this.labelProductName.Text = AssemblyProduct;
+      this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+      this.labelCopyright.Text = AssemblyCopyright;
+      this.labelCompanyName.Text = AssemblyCompany;
+      this.textBoxDescription.Text = AssemblyDescription;
+    }
+
+    #region Accesseurs d'attribut de l'assembly
+
+    public string AssemblyTitle
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+        if (attributes.Length > 0)
+        {
+          AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+          if (titleAttribute.Title != "")
+          {
+            return titleAttribute.Title;
+          }
+        }
+        return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+      }
+    }
+
+    public string AssemblyVersion
+    {
+      get
+      {
+        return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+      }
+    }
+
+    public string AssemblyDescription
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+      }
+    }
+
+    public string AssemblyProduct
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyProductAttribute)attributes[0]).Product;
+      }
+    }
+
+    public string AssemblyCopyright
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+      }
+    }
+
+    public string AssemblyCompany
+    {
+      get
+      {
+        object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+        if (attributes.Length == 0)
+        {
+          return "";
+        }
+        return ((AssemblyCompanyAttribute)attributes[0]).Company;
+      }
+    }
+    #endregion
+
+    private void okButton_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+  }
+}
---
+++ WinFormTemplate/FormMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+using WinFormTemplate.Properties;
+
+namespace WinFormTemplate
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void QuitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      AboutBoxApplication aboutBoxApplication = new AboutBoxApplication();
+      aboutBoxApplication.ShowDialog();
+    }
+
+    private void DisplayTitle()
+    {
+      Assembly assembly = Assembly.GetExecutingAssembly();
+      FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+      Text += string.Format(" V{0}.{1}.{2}.{3}", fvi.FileMajorPart, fvi.FileMinorPart, fvi.FileBuildPart, fvi.FilePrivatePart);
+    }
+
+    private void FormMain_Load(object sender, EventArgs e)
+    {
+      DisplayTitle();
+      GetWindowValue();
+    }
+
+    private void GetWindowValue()
+    {
+      // Set this if you want a minimum width
+      //Width = Settings.Default.WindowWidth < 395 ? 395 : Settings.Default.WindowWidth;
+      Width = Settings.Default.WindowWidth;
+      // Set this if you want a minimum Height
+      // Height = Settings.Default.WindowHeight < 180 ? 180 : Settings.Default.WindowHeight;
+      Height = Settings.Default.WindowHeight;
+      Top = Settings.Default.WindowTop < 0 ? 0 : Settings.Default.WindowTop;
+      Left = Settings.Default.WindowLeft < 0 ? 0 : Settings.Default.WindowLeft;
+    }
+
+    private void SaveWindowValue()
+    {
+      Settings.Default.WindowHeight = Height;
+      Settings.Default.WindowWidth = Width;
+      Settings.Default.WindowLeft = Left;
+      Settings.Default.WindowTop = Top;
+      Settings.Default.Save();
+    }
+
+    private void FormMainFormClosing(object sender, FormClosingEventArgs e)
+    {
+      SaveWindowValue();
+    }
+  }
+}
