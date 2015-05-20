60d4933defef860a3797d778932041d0211c2b85
Freddy Juhel <domaine75@gmail.com>
2015-05-20 14:00:19 +0200

    Add last language used on application exit

--- WinFormTemplate/Properties/AssemblyInfo.cs
+++ WinFormTemplate/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("WinFormTemplate")]
-[assembly: AssemblyDescription("Application")]
+[assembly: AssemblyDescription("Simple application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Freddy Juhel")]
 [assembly: AssemblyProduct("WinFormTemplate")]
