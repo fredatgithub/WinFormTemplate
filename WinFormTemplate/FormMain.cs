956ca0fd154fdb42ac5865910aa8cbed517604dc
fred <domaine75@gmail.com>
2015-12-25 00:05:40 +0100

    Update FormMain.cs

--- WinFormTemplate/FormMain.cs
+++ WinFormTemplate/FormMain.cs
@@ -545,7 +545,7 @@ namespace WinFormTemplate
     {
       if (tb != ActiveControl) return;
       var selectionIndex = tb.SelectionStart;
-      tb.Text = tb.Text.Insert(selectionIndex, Clipboard.GetText());
+      tb.SelectedText = Clipboard.GetText();
       tb.SelectionStart = selectionIndex + Clipboard.GetText().Length;
     }
 
